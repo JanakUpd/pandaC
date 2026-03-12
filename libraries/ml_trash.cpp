d3b7fbde76f0f3be547799474c42e20427b2bb93
Moon <mooninfinityalways@gmail.com>
2026-03-12 19:22:43 +0300

    vectors arithmetic

--- libraries/ml_trash.cpp
+++ libraries/ml_trash.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <cstdint>
 #include <stdexcept>
+#include <unordered_map>
 
 using Vector = std::vector<double>;
 using Matrix = std::vector<Vector>;
@@ -22,10 +23,37 @@ void validate_supervised_dataset(const Matrix& X, const Vector& y) {
     if (X.size() != y.size()) throw std::invalid_argument("X and y size unequal");
 }
 
-double dot(const Vector& a, const Vector& b) {
-    if (a.size() != b.size()) throw std::invalid_argument("dot: size of vectors unequal");
+double dot_product(const Vector& a, const Vector& b) {
+    if (a.size() != b.size()) throw std::invalid_argument("size of vectors in dot_product unequal");
     double result = 0.0;
     for (std::size_t i = 0; i < a.size(); i++) result += a[i] * b[i];
+    return result;
+}
+
+Vector add(const Vector& a, const Vector& b) {
+    if (a.size() != b.size()) throw std::invalid_argument("size of vectors in addiotion unequal");
+    Vector result(a.size());
+    for (std::size_t i = 0; i < a.size(); i++) result[i] = a[i] + b[i];
+    return result;
+}
+
+Vector subtract(const Vector& a, const Vector& b) {
+    if (a.size() != b.size()) throw std::invalid_argument("size of vectors in substriction unequal");
+    Vector result(a.size());
+    for (std::size_t i = 0; i < a.size(); ++i) result[i] = a[i] - b[i];
+    return result;
+}
+
+Vector multiply(const Vector& v, double scalar) {
+    Vector result(v.size());
+    for (std::size_t i = 0; i < v.size(); i++) result[i] = v[i] * scalar;
+    return result;
+}
+
+Vector divide(const Vector& v, double scalar) {
+    if (std::abs(scalar) < 1e-15) throw std::invalid_argument("scalar in dividition is zero");
+    Vector result(v.size());
+    for (std::size_t i = 0; i < v.size(); ++i) result[i] = v[i] / scalar;
     return result;
 }
 
