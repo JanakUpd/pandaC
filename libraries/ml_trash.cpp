cc510d314a096f9df386f528297107543260de3a
Moon <mooninfinityalways@gmail.com>
2026-03-12 19:04:02 +0300

    validation errors

--- libraries/ml_trash.cpp
+++ libraries/ml_trash.cpp
@@ -4,65 +4,26 @@
 #include <random>
 #include <cstdint>
 
-using vd = std::vector<double>;
-using vdd = std::vector<vd>;
+using Vector = std::vector<double>;
+using Matrix = std::vector<Vector>;
 
-class LogisticRegression {
-private:
-    vd weights;
-    uint64_t m;
-    double b;
-    double sigmoid(double z) {
-        return 1.0 / (1.0 + exp(-z));
+void validate_matrix(const Matrix& X) {
+    if (X.empty()) throw std::invalid_argument("Matrix is empty");
+    const std::size_t m = X[0].size();
+    if (m == 0) throw std::invalid_argument("Matrix has zero columns");
+    for (const auto& row : X) {
+        if (row.size() != m) throw std::invalid_argument("Matrix rows have different sizes");
     }
-public:
-    LogisticRegression(int features) {
-        m = features;
-        weights.resize(m);
-        b = 0.0;
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<> dis(-0.01, 0.01);
-        for (uint64_t i = 0; i < m; i++) weights[i] = dis(gen);
-    }
-    void fit(const vdd& X, const vd& y, uint64_t epochs, double learning_rate) {
-        uint64_t n = X.size();
-        vd dw(m, 0.0);
-        double db = 0.0;
-        for (uint64_t epoch = 0; epoch < epochs; epoch++) {
-            vd dw(m, 0.0);
-            double db = 0.0;
-            for (int i = 0; i < n; i++) {
-                double z = b;
-                for (uint64_t j = 0; j < m; j++) z += weights[j] * X[i][j];
-                double y_hat = sigmoid(z);
-                double error = y_hat - y[i];
-                for (int j = 0; j < m; j++) dw[j] += error * X[i][j];
-                db += error;
-            }
-            for (int j = 0; j < m; j++) weights[j] -= learning_rate * dw[j] / n;
-            b -= learning_rate * db / n;
-        }
-    }
-    double predict_double(const vd& x) {
-        double z = b;
-        for (uint64_t j = 0; j < m; j++) z += weights[j] * x[j];
-        return sigmoid(z);
-    }
-    int predict(const vd& x) {
-        return predict_double(x) >= 0.5 ? 1 : 0;
-    }
-};
+}
 
-class ILoss {
-public:
-    virtual ~ILoss() = default;
-    virtual double value(const vd& y_true, const vd& y_pred) const = 0;
-    virtual vd gradient(const vd& y_true, const vd& y_pred) const = 0;
-};
+void validate_supervised_dataset(const Matrix& X, const Vector& y) {
+    validate_matrix(X);
+    if (X.size() != y.size()) throw std::invalid_argument("X and y size unequal");
+}
 
-class BinaryCrossEntropy final : public ILoss {
-public:
-    double value(const vd& y_true, const vd& y_pred) const override {}
-    vd gradient(const vd& y_true, const vd& y_pred) const override {}
-};
+double dot(const Vector& a, const Vector& b) {
+    if (a.size() != b.size()) throw std::invalid_argument("dot: size of vectors unequal");
+    double result = 0.0;
+    for (std::size_t i = 0; i < a.size(); i++) result += a[i] * b[i];
+    return result;
+}
